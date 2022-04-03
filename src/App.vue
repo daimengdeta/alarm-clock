5dbd8aa01f967de4eee4033dea603d2a6ba91a9e
xlz <20794489@qq.com>
2022-04-04 00:03:19 +0800

    记忆点击的哪一个练习目录，避免刷新页面后回到第一个目录

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,57 @@
 <template>
   <div class="header-nav">
     <div class="nav-item" :class="isActive(item)" v-for="item in comps" :key="item" @click="navTo(item)">
-      {{item}}
+      {{ item }}
     </div>
   </div>
   <component :is="curComp" />
 </template>
 
-
 <script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import 父子传值 from './test/父子传值/index.vue'
-import 列表练习 from './test/列表练习/index.vue'
-import 子传父练习 from './test/子传父练习/index.vue'
+import 父子传值 from './test/父子传值/index.vue';
+import 列表练习 from './test/列表练习/index.vue';
+import 子传父练习 from './test/子传父练习/index.vue';
 
 export default {
-  components: {父子传值, 列表练习, 子传父练习},
+  components: { 父子传值, 列表练习, 子传父练习 },
   data() {
     return {
       comps: ['父子传值', '列表练习', '子传父练习'],
-      curComp: ''
-    }
+      curComp: '',
+    };
   },
   methods: {
     navTo(item) {
-      this.curComp = item
+      this.curComp = item;
+      location.hash = '/' + item;
+      console.log(location);
     },
-    isActive(item){
-      if(this.curComp===item){
-        return 'active'
+    isActive(item) {
+      if (this.curComp === item) {
+        return 'active';
       }
-      return ''
-
+      return '';
     },
   },
   mounted() {
-    this.curComp = this.comps[0]
+    const hash = location.hash;
+    //去掉网址前面的#/
+    if (hash) {
+      const curComp = hash.substring(2);
+      //点击哪个就保存哪个
+      this.curComp = decodeURIComponent(curComp);
+
+      console.log(decodeURIComponent(curComp), '===========打印的 ------ mounted');
+    } else {
+      //点击哪个就保存哪个
+      this.curComp = this.comps[0];
+    }
+    console.log(hash);
   },
-}
+};
 </script>
-
-
 
 <style lang="less">
 #app {
@@ -52,21 +62,20 @@ export default {
   color: #2c3e50;
   margin-top: 10px;
 }
-.header-nav{
+.header-nav {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-bottom: 20px;
-  .nav-item{
+  .nav-item {
     cursor: pointer;
     margin-left: 20px;
-    &:hover{
+    &:hover {
       color: #73c9e5;
     }
   }
 }
-.active{
+.active {
   color: #73c9e5;
 }
-
 </style>

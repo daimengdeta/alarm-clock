28a8d40d54ca25929aa54f81941f08f87a6e86b5
xlz <20794489@qq.com>
2022-04-06 00:41:30 +0800

    feat: 父传子结合双向绑定

---
+++ src/test/父传子结合双向绑定/components/child.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="child1">
+    <div class="">子1：{{ num1 }}</div>
+    <input type="text" :value="num1" @input="number1" />
+    <div class="">子2：{{ num2 }}</div>
+    <input type="text" :value="num2" @input="number2" />
+    <div class="">obj子：{{ obj?.num }}</div>
+    <input type="text" v-model="obj.num" />
+    <div class="">array子：{{ array?.[0] }}</div>
+    <input type="text" v-model="array[0]" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg', 'num1', 'num2', 'obj', 'array'],
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    number1(evt) {
+      const value = evt.target.value;
+      this.$emit('xlz', value);
+    },
+    number2(evt) {
+      console.log(evt, '===========打印的 ------ number2');
+      const value = evt.target.value;
+      console.log(value, '===========打印的 ------ number2');
+      this.$emit('xlz2', value);
+      // console.log(num2, '===========打印的 ------ number1')
+      // this.$emit('xlz2', this.num2);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.child1 {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
---
+++ src/test/父传子结合双向绑定/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>父传子结合双向绑定</h1>
+  <div class="main">
+    <div class="">
+      <div class="">父1：{{ num1 }}</div>
+      <div class="">父2：{{ num2 }}</div>
+      <div class="">父obj：{{ obj.num }}</div>
+      <div class="">父array：{{ array[0] }}</div>
+    </div>
+    <child :num1="num1" :num2="num2" :obj="obj" :array="array" @xlz="xlz" @xlz2="xlz2"></child>
+  </div>
+
+  <div class="requirement"></div>
+  <div class="">
+    要求：<br />1.父组件传给子组件， <br />2.子组件输入内容可以改变子组件的值和父组件的值 <br />3.子组件是父组件的100被
+  </div>
+</template>
+
+<script>
+import Child from './components/child.vue';
+export default {
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      num1: 1,
+      num2: 1,
+      obj: {
+        num: 100,
+      },
+      array: [999],
+    };
+  },
+  methods: {
+    xlz(val) {
+      this.num1 = val;
+    },
+    xlz2(val) {
+      this.num2 = val;
+      console.log(val, '===========打印的 ------ val');
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+a {
+  color: #42b983;
+}
+.requirement {
+  border-top: red solid 1px;
+}
+.main {
+  display: flex;
+  width: 60%;
+  margin: auto;
+  justify-content: space-between;
+}
+</style>
